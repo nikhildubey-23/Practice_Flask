669f46bbfa8a7a076dddfe5be72c1a23f4ccd97f
nikhildubey-23 <ravannick0089@gmail.com>
2024-10-26 20:58:39 +0530

    removed the unwanted files

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ def hello_world():
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True,port=8888)
+@app.route("/about")
+def about():
+    return render_template("about.html")
